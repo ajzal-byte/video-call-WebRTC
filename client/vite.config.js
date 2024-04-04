deb966cf2614559be0d72cc08d199b3594da0286
ajzal-byte <ajzalaju10@gmail.com>
2024-04-05 05:05:45 +0530

    unwanted stuff removed

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,18 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// Add the alias for the crypto library
-const alias = {
-  crypto: "crypto-browserify",
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
   define: {
     global: {},
-  },
-  resolve: {
-    alias,
   },
 });
