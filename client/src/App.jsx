69c90ea841f12ba64f1e24ac03c6eb62737a8aa3
ajzal-byte <ajzalaju10@gmail.com>
2024-04-05 01:19:15 +0530

    video played

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { VideoPlayer, Notifications, Options } from "./components";
+
+import { AppBar, Typography, Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const StyledAppBar = styled(AppBar)(({theme}) => ({
+  borderRadius: 15,
+  margin: "30px 100px",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "600px",
+  border: "2px solid black",
+
+  [theme.breakpoints.down("xs")]: {
+    width: "90%",
+  },
+}));
+
+const StyledWrapper = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "100%",
+});
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <StyledWrapper>
+      <StyledAppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Call App
+        </Typography>
+      </StyledAppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </StyledWrapper>
+  );
+};
 
-export default App
+export default App;
