e0a81961ecd441112099c24a808ccccf0eb6d23b
ajzal-byte <ajzalaju10@gmail.com>
2024-04-05 05:35:53 +0530

    api url updated

--- frontend/src/SocketContext.jsx
+++ frontend/src/SocketContext.jsx
@@ -5,7 +5,8 @@ import { toast } from "react-hot-toast";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:5000");
+// const socket = io("http://localhost:5000");
+const socket = io("https://video-call-webrtc-gq51.onrender.com");
 
 const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState(null);
